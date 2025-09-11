8d49e6a6d9e8f5dd4686c3fb51cd9b0b2d392bfb
nh <nickxhan@umich.edu>
2025-09-10 21:42:31 -0400

    Add base styling and HTML elements

---
+++ index.js
@@ -0,0 +1,6 @@
+import weatherHandler from "./handler.js";
+
+const displays = document.querySelectorAll(".display");
+console.log(displays);
+// const x = await weatherHandler.getLocationWeatherData("london");
+// console.log(x);
