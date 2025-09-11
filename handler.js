793104c223ac65f31b3d333e0f8e0cf69031ca6f
nh <nickxhan@umich.edu>
2025-09-10 23:18:41 -0400

    Add searchbar functionality

--- handler.js
+++ handler.js
@@ -1,14 +1,36 @@
 const API_KEY = "2SVD9LX4P99P5TDBUWYCFZ59V";
 
 const weatherHandler = (() => {
-    async function getLocationWeatherData(locationInput) {
-        const location = locationInput;
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`, { mode: 'cors' });
-        const json = await response.json();
-        return json;
+    async function makeWeatherQuery(locationInput) {
+        const locations = [encodeURIComponent(locationInput), "london"];
+
+        for (const location of locations) {
+            try {
+                let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`);
+                if (!response.ok) {
+                    console.warn(`Failed for ${location}, trying fallback`);
+                    continue;
+                }
+                const json = await response.json();
+                return json;
+            }
+            catch (error) {
+                console.warn(`Failed for ${location}:`, error.message);
+            }
+        }
+
+        throw new Error('Could not fetch weather data from any location');
+    }
+    function getCurrentConditionFieldNames() {
+        const fields = ["temperature", "feelslike", "datetime", "conditions", "source"];
+        return fields;
+    }
+    function getOtherFieldNames() {
+        const fields = ["latitude", "longitude"];
+        return fields;
     }
 
-    return { getLocationWeatherData };
+    return { makeWeatherQuery, getCurrentConditionFieldNames, getOtherFieldNames };
 })();
 
 
--- index.js
+++ index.js
@@ -2,5 +2,13 @@ import weatherHandler from "./handler.js";
 
 const displays = document.querySelectorAll(".display");
 console.log(displays);
-// const x = await weatherHandler.getLocationWeatherData("london");
-// console.log(x);
+
+const locationForm = document.querySelector("form#locationForm");
+const searchbar = document.querySelector("#search");
+
+locationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const location = searchbar.value;
+    weatherHandler.makeWeatherQuery(location);
+})
+
